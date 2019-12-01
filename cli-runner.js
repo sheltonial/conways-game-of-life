0b64e92147efbdc8b1074ba3bc70d8629416746b
Andrew Shelton <andrew.shelton@klarna.com>
2019-12-01 19:04:02 +0100

    prettier formatted

--- cli-runner.js
+++ cli-runner.js
@@ -1,40 +1,39 @@
-const gol = require('./lib/gol')
-const table = require('table').table
-const term = require( 'terminal-kit' ).terminal
-const emoji = require('node-emoji')
+const gol = require("./lib/gol");
+const table = require("table").table;
+const term = require("terminal-kit").terminal;
+const emoji = require("node-emoji");
 
 const args = process.argv.slice(2);
-const maxGenerations = parseInt(args[0])
-const matrixSize = parseInt(args[1])
-const timeoutMs = parseInt(args[2])
-const startingCoords = JSON.parse(args[3])
+const maxGenerations = parseInt(args[0]);
+const matrixSize = parseInt(args[1]);
+const timeoutMs = parseInt(args[2]);
+const startingCoords = JSON.parse(args[3]);
 
 function renderMatrix(coords, size) {
-    const matrix = gol.coordsToMatrix(coords, size, emoji.get('smile'))
-    const tableConfig = {
-        columnDefault: {
-            width: 3
-        }
-    };
-    term.moveTo(1, 1)
-    term(table(matrix, tableConfig));
+  const matrix = gol.coordsToMatrix(coords, size, emoji.get("smile"));
+  const tableConfig = {
+    columnDefault: {
+      width: 3
+    }
+  };
+  term.moveTo(1, 1);
+  term(table(matrix, tableConfig));
 }
 
 function play(startingCoords) {
-    let coords = startingCoords
-    const outputPadding = '    '
+  let coords = startingCoords;
+  const outputPadding = "    ";
 
-    renderMatrix(coords, matrixSize)
+  renderMatrix(coords, matrixSize);
 
-    for (let i = 1; i <= maxGenerations; i ++) {
-        setTimeout(() => {
-            coords = gol.nextGeneration(coords)
-            renderMatrix(coords, matrixSize)
-            console.log(`Generation: ${i}${outputPadding}`)
-            console.log(`Alive nodes: ${coords.length}${outputPadding}`)
-
-        }, timeoutMs * i)
-    }
+  for (let i = 1; i <= maxGenerations; i++) {
+    setTimeout(() => {
+      coords = gol.nextGeneration(coords);
+      renderMatrix(coords, matrixSize);
+      console.log(`Generation: ${i}${outputPadding}`);
+      console.log(`Alive nodes: ${coords.length}${outputPadding}`);
+    }, timeoutMs * i);
+  }
 }
 
-play(startingCoords)
+play(startingCoords);
