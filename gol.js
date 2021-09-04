78d30203009ce926dccb4c1e50ba2cd9c04603ba
sheltonial <sheltonial@users.noreply.github.com>
2021-09-04 13:04:09 +0000

    deploy: 6f863d9849cf30eca031b21f59d6bd054d5bbc45

--- gol.js
+++ gol.js
@@ -1,109 +1,111 @@
-const CELL_STATE = {
-  DEAD: 0,
-  ALIVE: 1,
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-
+var CellState;
+(function (CellState) {
+    CellState[CellState["Dead"] = 0] = "Dead";
+    CellState[CellState["Alive"] = 1] = "Alive";
+})(CellState || (CellState = {}));
 function coordsToStateKey(x, y) {
-  return `${x}_${y}`;
+    return x + "_" + y;
 }
-
 function stateKeyToCoords(key) {
-  const keySplit = key.split("_");
-  return { x: parseInt(keySplit[0]), y: parseInt(keySplit[1]) };
+    var _a = key.split("_").map(function (i) { return parseInt(i); }), x = _a[0], y = _a[1];
+    return { x: x, y: y };
 }
-
 function isCellAlive(x, y, state) {
-  return state[coordsToStateKey(x, y)] === CELL_STATE.ALIVE;
+    return state[coordsToStateKey(x, y)] === CellState.Alive;
 }
-
-function setCell(x, y, state, isAlive) {
-  state[coordsToStateKey(x, y)] = isAlive ? CELL_STATE.ALIVE : CELL_STATE.DEAD;
+function trackCell(x, y, state, isAlive) {
+    state[coordsToStateKey(x, y)] = isAlive ? CellState.Alive : CellState.Dead;
 }
-
-function getNeighbourCoords(x, y) {
-  const coords = [];
-  for (let i = x - 1; i <= x + 1; i++) {
-    for (let j = y - 1; j <= y + 1; j++) {
-      if (!(i === x && j === y)) {
-        coords.push({ x: i, y: j });
-      }
+function getNeighborCoords(x, y) {
+    var coords = [];
+    for (var i = x - 1; i <= x + 1; i++) {
+        for (var j = y - 1; j <= y + 1; j++) {
+            if (!(i === x && j === y)) {
+                coords.push({ x: i, y: j });
+            }
+        }
     }
-  }
-  return coords;
+    return coords;
 }
-
 function countAliveNeighbors(x, y, state) {
-  const neighbourCoords = getNeighbourCoords(x, y);
-  return neighbourCoords.reduce((count, { x, y }) => {
-    return count + (isCellAlive(x, y, state) ? 1 : 0);
-  }, 0);
+    var neighborCoords = getNeighborCoords(x, y);
+    return neighborCoords.reduce(function (count, _a) {
+        var x = _a.x, y = _a.y;
+        return count + (isCellAlive(x, y, state) ? 1 : 0);
+    }, 0);
 }
-
 function removeDeadCells(state) {
-  const newState = Object.assign({}, state);
-  for (let key in state) {
-    if (state[key] === CELL_STATE.DEAD) {
-      delete newState[key];
+    var newState = __assign({}, state);
+    for (var key in state) {
+        var a = state[0];
+        if (state[key] === CellState.Dead) {
+            delete newState[key];
+        }
     }
-  }
-  return newState;
+    return newState;
 }
-
 function trackNeighborsOfAliveCells(state) {
-  let newState = Object.assign({}, state);
-  for (let key in state) {
-    const coords = stateKeyToCoords(key);
-    const neighbourCoords = getNeighbourCoords(coords.x, coords.y);
-    neighbourCoords.forEach(({ x, y }) => {
-      const cellAlive = isCellAlive(x, y, state);
-      setCell(x, y, newState, cellAlive);
-    });
-  }
-  return newState;
+    var newState = __assign({}, state);
+    for (var key in state) {
+        var coords = stateKeyToCoords(key);
+        var neighborCoords = getNeighborCoords(coords.x, coords.y);
+        neighborCoords.forEach(function (_a) {
+            var x = _a.x, y = _a.y;
+            var cellAlive = isCellAlive(x, y, state);
+            trackCell(x, y, newState, cellAlive);
+        });
+    }
+    return newState;
 }
-
 function shouldBeAlive(isCurrentlyAlive, aliveNeighbors) {
-  if (aliveNeighbors < 2 || aliveNeighbors > 3) {
+    if (aliveNeighbors < 2 || aliveNeighbors > 3) {
+        return false;
+    }
+    else if (isCurrentlyAlive) {
+        return true;
+    }
+    else if (aliveNeighbors === 3 && !isCurrentlyAlive) {
+        return true;
+    }
     return false;
-  } else if (isCurrentlyAlive) {
-    return true;
-  } else if (aliveNeighbors === 3 && !isCurrentlyAlive) {
-    return true;
-  }
-  return false;
 }
-
 function coordsToState(coords) {
-  return coords.reduce((acc, cur) => {
-    acc[coordsToStateKey(cur[0], cur[1])] = CELL_STATE.ALIVE;
-    return acc;
-  }, {});
+    return coords.reduce(function (acc, cur) {
+        acc[coordsToStateKey(cur[0], cur[1])] = CellState.Alive;
+        return acc;
+    }, {});
 }
-
 function stateToCoords(state) {
-  return Object.keys(state).reduce((acc, key) => {
-    const { x, y } = stateKeyToCoords(key);
-    acc.push([x, y]);
-    return acc;
-  }, []);
+    return Object.keys(state).reduce(function (acc, key) {
+        var _a = stateKeyToCoords(key), x = _a.x, y = _a.y;
+        acc.push([x, y]);
+        return acc;
+    }, []);
 }
-
 function nextGeneration(coords) {
-  const state = coordsToState(coords);
-  const stateTrackingNeighbors = trackNeighborsOfAliveCells(state);
-  let newState = {};
-
-  for (let key in stateTrackingNeighbors) {
-    const { x, y } = stateKeyToCoords(key);
-    const aliveNeighbors = countAliveNeighbors(x, y, state);
-    const currentlyAlive = isCellAlive(x, y, state);
-    const alive = shouldBeAlive(currentlyAlive, aliveNeighbors);
-    setCell(x, y, newState, alive);
-  }
-
-  return stateToCoords(removeDeadCells(newState));
+    var state = coordsToState(coords);
+    var stateTrackingNeighbors = trackNeighborsOfAliveCells(state);
+    var newState = {};
+    for (var key in stateTrackingNeighbors) {
+        var _a = stateKeyToCoords(key), x = _a.x, y = _a.y;
+        var aliveNeighbors = countAliveNeighbors(x, y, state);
+        var currentlyAlive = isCellAlive(x, y, state);
+        var alive = shouldBeAlive(currentlyAlive, aliveNeighbors);
+        trackCell(x, y, newState, alive);
+    }
+    return stateToCoords(removeDeadCells(newState));
 }
-
 /**
  * Filters coordinates to a specified viewport.
  * @param {int} x - Starting x position of the viewport.
@@ -112,22 +114,18 @@ function nextGeneration(coords) {
  * @param {int} height - Height of viewport.
  */
 function filterCoordsInViewport(coords, x, y, width, height) {
-  const result = [];
-
-  let zeroOffsetX = x * -1;
-  let zeroOffsetY = y * -1;
-
-  coords.forEach((coord) => {
-    const x1 = coord[0];
-    const y1 = coord[1];
-
-    if (x1 >= x && y1 >= y && x1 < width + x && y1 < height + y) {
-      result.push([x1 + zeroOffsetX, y1 + zeroOffsetY]);
-    }
-  });
-  return result;
+    var result = [];
+    var zeroOffsetX = x * -1;
+    var zeroOffsetY = y * -1;
+    coords.forEach(function (coord) {
+        var x1 = coord[0];
+        var y1 = coord[1];
+        if (x1 >= x && y1 >= y && x1 < width + x && y1 < height + y) {
+            result.push([x1 + zeroOffsetX, y1 + zeroOffsetY]);
+        }
+    });
+    return result;
 }
-
 /**
  * Creates a matrix for coordinates within a given viewport
  * @param {int} x - Starting x position of coordinates to include.
@@ -136,40 +134,35 @@ function filterCoordsInViewport(coords, x, y, width, height) {
  * @param {int} height - Height of matrix.
  */
 function coordsToMatrix(coords, x, y, width, height, activeChar) {
-  const coordsInViewport = filterCoordsInViewport(coords, x, y, width, height);
-
-  const matrix = [];
-  for (let y = 0; y < height; y++) {
-    matrix[y] = new Array(width);
-  }
-
-  coordsInViewport.forEach((coord) => {
-    const x1 = coord[0];
-    const y1 = coord[1];
-
-    matrix[y1][x1] = activeChar;
-  });
-
-  return matrix;
+    var coordsInViewport = filterCoordsInViewport(coords, x, y, width, height);
+    var matrix = [];
+    for (var y_1 = 0; y_1 < height; y_1++) {
+        matrix[y_1] = new Array(width);
+    }
+    coordsInViewport.forEach(function (coord) {
+        var x1 = coord[0];
+        var y1 = coord[1];
+        matrix[y1][x1] = activeChar;
+    });
+    return matrix;
 }
-
-golExports = {
-  coordsToStateKey,
-  stateKeyToCoords,
-  isCellAlive,
-  setCell,
-  getNeighbourCoords,
-  countAliveNeighbors,
-  removeDeadCells,
-  trackNeighborsOfAliveCells,
-  shouldBeAlive,
-  coordsToState,
-  stateToCoords,
-  nextGeneration,
-  filterCoordsInViewport,
-  coordsToMatrix,
+var golExports = {
+    coordsToStateKey: coordsToStateKey,
+    stateKeyToCoords: stateKeyToCoords,
+    isCellAlive: isCellAlive,
+    trackCell: trackCell,
+    getNeighborCoords: getNeighborCoords,
+    countAliveNeighbors: countAliveNeighbors,
+    removeDeadCells: removeDeadCells,
+    trackNeighborsOfAliveCells: trackNeighborsOfAliveCells,
+    shouldBeAlive: shouldBeAlive,
+    coordsToState: coordsToState,
+    stateToCoords: stateToCoords,
+    nextGeneration: nextGeneration,
+    filterCoordsInViewport: filterCoordsInViewport,
+    coordsToMatrix: coordsToMatrix
 };
-
 if (typeof module !== "undefined" && typeof module.exports !== "undefined")
-  module.exports = golExports;
-else window.gol = golExports;
+    module.exports = golExports;
+else
+    window["gol"] = golExports;
